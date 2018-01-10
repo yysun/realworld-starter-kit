df8451fd48397b9e2fb0a5301ae95550b68c9eb7
Yiyi Sun <yiyisun@gmail.com>
2018-01-09 20:56:00 -0500

    add note of new repository

--- src/components/error-list.tsx
+++
@@ -1,7 +0,0 @@
-import app from 'apprun'
-
-export default function ({ errors }) {
-  return <ul className="error-messages">
-    {Object.keys(errors).map(key => <li>{`${key} ${errors[key]}`}</li>)}
-  </ul>
-}
